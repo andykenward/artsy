a7d3e2f11012f31a16f756bb766916aa5b2346dc
Andy Kenward <hello@andykenward.com>
2019-07-21 10:01:21 +0100

    refactor: apollo client

---
+++ src/apollo-client.ts
@@ -0,0 +1,36 @@
+import ApolloClient from 'apollo-boost';
+
+export const client = new ApolloClient({
+  uri:
+    process.env.REACT_APP_API || 'https://metaphysics-production.artsy.net/v2/',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  request: async operation => {
+    const token = process.env.REACT_APP_USER_TOKEN;
+
+    operation.setContext({
+      headers: token
+        ? {
+            'x-access-token': token,
+          }
+        : {},
+    });
+  },
+  onError: ({ graphQLErrors, networkError }) => {
+    /**
+     * Server error responses are bad
+     * message response is bad formatted JSON
+     * there are no error codes to denote unauthorized user
+     */
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
+            locations
+          )}, Path: ${path}`
+        )
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const client = new ApolloClient({
-  uri:
-    process.env.REACT_APP_API || 'https://metaphysics-production.artsy.net/v2/',
-});
+import { client } from './apollo-client';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <ApolloHooksProvider client={client}>
-      <App />
-    </ApolloHooksProvider>
-  </ApolloProvider>,
+  <ApolloHooksProvider client={client}>
+    <App />
+  </ApolloHooksProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
