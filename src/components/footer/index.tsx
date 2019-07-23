2eb5ad574283f2cb5b3cb0d93a0c421ba9b965bb
Andy Kenward <hello@andykenward.com>
2019-07-23 11:39:14 +0100

    chore: header & footer clean up

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,9 +3,22 @@ import React from 'react';
 
 export const Footer: React.FC = () => (
   <Flex m="1rem" as="footer" flexDirection="column" alignItems="flex-start">
-    <Serif size={['5', '6']}>Andy Kenward</Serif>
-    <Link href="https://andykenward.com" target="_blank">
-      andykenward.com
+    <Serif size={['5', '6']}>
+      <strong>Andy Kenward</strong>
+    </Serif>
+    <Link
+      href="https://andykenward.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Serif size={['5', '6']}>andykenward.com</Serif>
+    </Link>
+    <Link
+      href="https://github.com/andykenward/ori-homework-artsy"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Serif size={['5', '6']}>GitHub</Serif>
     </Link>
   </Flex>
 );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,12 @@
-import { Flex, Serif } from '@artsy/palette';
+import { Flex, Link, Serif } from '@artsy/palette';
 import React from 'react';
 
 export const Header: React.FC = () => (
   <Flex m="1rem" as="header" flexDirection="column" alignItems="flex-start">
-    <Serif element="h2" size={['8', '10']}>
-      Artsy.net - GraphQL API
+    <Serif element="h1" size={['8', '10']} display="inline-flex">
+      <Link href="https://artsy.net" target="_blank" rel="noopener noreferrer">
+        <Serif size={['8', '10']}>Artsy.net</Serif>
+      </Link>
     </Serif>
   </Flex>
 );
