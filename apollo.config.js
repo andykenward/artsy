6e7326de1b1f7b9f6c259a5330451630e5c2a72f
Andy Kenward <hello@andykenward.com>
2019-08-05 16:23:00 +0100

    fix: apollo vscode config
    wasn’t checking query files

--- apollo.config.js
+++ apollo.config.js
@@ -3,6 +3,7 @@ const SCHEMA_PATH = process.env.SCHEMA_PATH || './data/schema.graphql';
 
 module.exports = {
   client: {
+    includes: ['./src/components/**/*.ts'],
     service: {
       name: 'artsy-metaphysics-v2',
       localSchemaFile: SCHEMA_PATH,
