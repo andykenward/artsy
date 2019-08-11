613f9d81ddac6efed6007827f2033141a409a5af
Andy Kenward <hello@andykenward.com>
2019-08-11 18:00:31 +0100

    chore: apollo + graphql updates

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,6 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect';
-// this will ensure they're removed after each test.
-import '@testing-library/react/cleanup-after-each';
 import 'jest-styled-components';
 
 export default undefined;
